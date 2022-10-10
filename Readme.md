9ac381d32d51da28d119ac70e4f8dd017e937a27
Stanislav Katkov <s.katkov@petrovich.ru>
2022-10-10 20:08:05 +0300

    fix_composer

--- Readme.md
+++ Readme.md
@@ -3,5 +3,8 @@
 ### Run docker container
 `docker-compose up -d`
 
+### Initial composer
+`docker exec -i social-php-fpm composer install`
+
 ### Run database migration
 `docker exec -i social-mysql mysql -uroot -psecret social < datadump.sql`
--- www/public/index.php
+++ www/public/index.php
@@ -5,8 +5,8 @@ use App\Base\Response;
 use App\Base\Router;
 
 require_once __DIR__.'/../vendor/autoload.php';
-require_once __DIR__.'/../bootstrap/bootstrap.php';
-require_once __DIR__.'/../config/routes.php';
+require_once __DIR__ . '/../bootstrap/bootstrap.php';
+require_once __DIR__ . '/../config/routes.php';
 
 
 $response = Router::prepare(new Request(), new Response());
